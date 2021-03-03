245fd0dea03ccab2cf13b011999c9ad57fe167e0
nova-sangeeth <novasangeeth@outlook.com>
2021-03-03 15:27:13 +0530

    removed all the items from the main app.vue file and added it to separate components.

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,47 +1,19 @@
 <template>
   <div id="app">
-    <p>{{ user.username }}</p>
+    <user-profile/>
+    <!-- <tweets/> -->
   </div>
 </template>
 
 <script>
-
+// import tweets from './components/Tweet.vue'
+import UserProfile from './components/userProfile.vue'
 export default {
   name: "App",
-  data() {
-    return {
-      followers: 0,
-      user: {
-        id: 1,
-        username: "nova",
-        firstName: "Nova",
-        lastName: "Sangeeth",
-        email: "novasangeeth@gg.com",
-        isAdmin: false,
-      },
-    };
+  components: {
+    // tweets, 
+    UserProfile,
   },
-  computed: {
-    fullName() {
-      return `${this.user.firstName} ${this.user.lastName}`;
-    },
-  },
-  methods: {
-    followUser() {
-      this.followers = this.followers + 1
-    }
-  },
-  mounted() {
-    this.followUser();
-    console.log("this function is mounted right now and the follow is appended by 1.")
-  },
-  watch: {
-    followers(newfollowerCount, old_followerCount) {
-      if (old_followerCount < newfollowerCount) {
-        console.log(this.user.username + " has gained a follower")
-      }
-    }
-  }
 };
 </script>
 
@@ -50,8 +22,10 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  background-color: None;
+  text-align: justify;
+  color: #4d2c50;
   margin-top: 60px;
+  padding: 25px;
 }
 </style>
