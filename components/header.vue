4760cab1b5b95d8c13692d7aaa7a4b0c1da49735
nova_sangeeth <novasangeeth@outlook.com>
2021-10-13 21:57:29 +0530

    restructure

---
+++ components/header.vue
@@ -0,0 +1,37 @@
+<template>
+  <nav class="header link-spacing">
+
+    <div class="header-btn-right link-spacing">
+      <a href="" class="link-spacing">Home</a>
+      <a href="" class="link-spacing">login</a>
+      <a href="" class="link-spacing">sign-up</a>
+      <a href="" class="link-spacing">contact</a>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.header-btn-right {
+  float: right;
+}
+.header-logo-left {
+  float: left;
+}
+.header {
+  display: flex;
+  flex-basis: auto;
+  background-color: #d4d4d4;
+  border-radius: 10px;
+  flex-grow: 1;
+  -ms-flex-align: center;
+  align-items: center;
+  height: 100%;
+}
+.link-spacing {
+  margin: 10px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <new-header/>
+  <!-- <login/> -->
+
+</template>
+
+<script>
+import header from "../components/header.vue";
+import login from "../components/login.vue";
+export default {
+  components: {
+    "new-header": header,
+    "login": login,
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
+
+
