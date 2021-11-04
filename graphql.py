2f4d43702730b649cc299d074d42425a4cfdf888
nova_sangeeth <novasangeeth@outlook.com>
2021-11-04 23:28:02 +0530

    basic mutations.

---
+++ graphql.py
@@ -0,0 +1,44 @@
+import graphene
+from graphene import String
+from graphql import GraphQLError
+from starlette.graphql import GraphQLApp
+
+class CreateUser(graphene.Mutation):
+    class Arguments: 
+        username = String(required=True)
+        password = String(required=True)
+        email = String(required=True)
+        first_name = String(required=True)
+        last_name = String(required=False)
+
+        ok = graphene.Boolean()
+
+        @staticmethod
+        def mutate(self, info, username, password, email, first_name, last_name):
+            ok = True
+            return CreateUser(ok=ok)
+
+
+class AuthenticateUser(graphene.Mutation):
+    class Arguments: 
+        username = String(required=True)
+        password = String(required=True)
+
+        ok = graphene.Boolean()
+
+        @staticmethod
+        def mutate(self, info, username, password):
+            ok = True
+            return AuthenticateUser(ok=ok)
+
+
+class CreateNewBlog(graphene.Mutation):
+    class Arguments: 
+        title = String(required=True)
+        content = String(required=True)
+
+        ok = graphene.Boolean()
+
+        @staticmethod
+        def mutate(self, info, title, content):
+            ok = True
