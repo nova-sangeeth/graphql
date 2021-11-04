5066abfea805804b39ede76d22e0b1da05dcb910
nova_sangeeth <novasangeeth@outlook.com>
2021-11-05 00:02:41 +0530

    models. added.

---
+++ models.py
@@ -0,0 +1,27 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from db import Base
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    username = Column(String)
+    email = Column(String)
+    password = Column(String)
+    first_name = Column(String)
+    last_name = Column(String)
+
+    def __init__(self, name=None, email=None):
+        self.name = name
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % (self.name)
+
+
+class Blog(Base):
+    __tablename__ = 'blogs'
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    content = Column(String)
+    created_by = Column(Integer, ForeignKey('users.id'))
+    
