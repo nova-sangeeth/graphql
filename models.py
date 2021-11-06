894d40b364a997276a0c1c6c43e37a16a249a853
nova_sangeeth <novasangeeth@outlook.com>
2021-11-06 23:50:56 +0530

    formated the code.

--- models.py
+++ models.py
@@ -1,8 +1,9 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from db import Base
 
+
 class User(Base):
-    __tablename__ = 'users'
+    __tablename__ = "users"
     id = Column(Integer, primary_key=True)
     username = Column(String)
     email = Column(String)
@@ -15,13 +16,12 @@ class User(Base):
         self.email = email
 
     def __repr__(self):
-        return '<User %r>' % (self.name)
+        return "<User %r>" % (self.name)
 
 
 class Blog(Base):
-    __tablename__ = 'blogs'
+    __tablename__ = "blogs"
     id = Column(Integer, primary_key=True)
     title = Column(String)
     content = Column(String)
-    created_by = Column(Integer, ForeignKey('users.id'))
-    
+    created_by = Column(Integer, ForeignKey("users.id"))
