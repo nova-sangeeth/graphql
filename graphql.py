e6d7c44557990d44f3e2b01a76e92dd804fb8033
nova_sangeeth <novasangeeth@outlook.com>
2021-11-11 19:22:43 +0530

    working on changes for graphql mutations for Creating a user.

--- graphql.py
+++ graphql.py
@@ -5,22 +5,37 @@ from graphql import GraphQLError
 from starlette.graphql import GraphQLApp
 
 
+
+import user_schema
+import blog_schema
+import crud
+import db as database_connection
+
+db_session=database_connection.db_session.session_factory()
+
 class CreateUser(graphene.Mutation):
     class Arguments:
-        username = String(required=True)
-        password = String(required=True)
-        email = String(required=True)
-        first_name = String(required=True)
-        last_name = String(required=False)
-        
-    ok = graphene.Boolean()
-    user graphene.Field()
+        username = graphene.String(required=True)
+        password = graphene.String(required=True)
+        email = graphene.String(required=True)
+        first_name = graphene.String(required=True)
+        last_name = graphene.String(required=False)
 
-        @staticmethod
-        def mutate(self, info, username, password, email, first_name, last_name):
-            hashed_password = bycrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-            ok = True
-            return CreateUser(ok=ok)
+
+    ok = graphene.Boolean()
+    user = graphene.Field(lambda: user_schema.UserInformation)
+
+    @staticmethod
+    def mutate(root, info, username, password, fullname, ):
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+        user = user_schema.UserInformation(username=username, password=hashed_password, fullname=fullname)
+        ok = True
+        db_user = crud.get_user_by_username(db_session, username=username)
+        if db_user:
+            raise GraphQLError("Username already exists")
+        user_info = user_schema.UserCreate(username=username, password=password, fullname=fullname)
+        crud.create_user(db_session, user_info)
+        return CreateUser(user=user, ok=ok)
 
 
 class AuthenticateUser(graphene.Mutation):
