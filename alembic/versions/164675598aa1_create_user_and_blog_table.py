c41156e9bd85b409b42e42e77441098877d78117
nova_sangeeth <novasangeeth@outlook.com>
2021-11-17 00:27:15 +0530

    new revision.

---
+++ alembic/versions/164675598aa1_create_user_and_blog_table.py
@@ -0,0 +1,24 @@
+"""create user and blog table.
+
+Revision ID: 164675598aa1
+Revises: 
+Create Date: 2021-11-16 23:40:04.992778
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '164675598aa1'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
